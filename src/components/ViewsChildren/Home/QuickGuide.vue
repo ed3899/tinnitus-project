cccd2feb67e0cf0b0e77638ea2cfcdcfe8e58da7
Edward Casanova <cb.inv7613@gmail.com>
2021-01-21 12:07:54 -0600

    image on quick guide

--- src/components/ViewsChildren/Home/QuickGuide.vue
+++ src/components/ViewsChildren/Home/QuickGuide.vue
@@ -1,5 +1,16 @@
 <template>
   <v-container class="temp-border">
+    <v-row>
+      <v-col>
+        <v-img
+          class="temp-border"
+          src="https://images.unsplash.com/photo-1446511437394-36cdff3ae1b3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&w=1000&q=80&h=300"
+          max-height="300"
+          max-width="1000"
+          contain
+        ></v-img>
+      </v-col>
+    </v-row>
     <v-row>Quick Guide</v-row>
   </v-container>
 </template>
