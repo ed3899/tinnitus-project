c5626f4f523b989d480eea9a63cb7ab32c29f446
Edward Casanova <cb.inv7613@gmail.com>
2021-01-27 09:35:54 -0600

    finish adding cards

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -20,28 +20,31 @@
         <v-col class="mt-5">
           <h1>ALL YOU NEED TO KNOW ABOUT TINNITUS</h1>
         </v-col>
-        <v-col class="mt-5">
-          <v-card max-width="300">
+        <v-col class="mt-5 d-flex justify-space-around">
+          <v-card
+            max-width="300"
+            v-for="{ title, text } in cards"
+            :key="title"
+            class="d-flex flex-column"
+          >
             <v-img
-              class="white--text align-end"
-              height="200px"
+              class="white--text"
+              max-height="200px"
               src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
             >
-              <v-card-title>What is tinnitus?</v-card-title>
+              <v-card-title v-text="title"></v-card-title>
             </v-img>
-            <v-card-text
-              >Here are some answers to commonly asked questions about tinnitus.
-              Tinnitus is often called ringing in the ears, but it can be
-              buzzing, hissing, whistling or any sound!</v-card-text
-            >
-            <v-card-actions>
-              <v-btn color="orange" text>
-                Read More
-              </v-btn>
 
-              <v-btn color="orange" text>
-                Share
-              </v-btn>
+            <v-card-text v-text="text" style="height: 150px;"></v-card-text>
+
+            <v-card-actions>
+              <v-btn
+                v-for="btn in btns"
+                :key="btn"
+                color="orange"
+                text
+                v-text="btn"
+              ></v-btn>
             </v-card-actions>
           </v-card>
         </v-col>
@@ -65,7 +68,32 @@ export default {
   components: {
     CarouselMain,
   },
-  data: () => ({}),
+  data: () => ({
+    cards: [
+      {
+        title: "What is tinnitus?",
+        text: `Here are some answers to commonly asked questions about tinnitus.
+              Tinnitus is often called ringing in the ears, but it can be
+              buzzing, hissing, whistling or any sound!`,
+      },
+      {
+        title: "What can I do about it?",
+        text: `There are several strategies that can be very helpful in managing tinnitus. Learn more about them here.`,
+      },
+      {
+        title: "How do I prevent it?",
+        text: `Tinnitus can occur for different reasons, but sometimes it is preventable. Read on to find out what prevention measures you can take.`,
+      },
+      {
+        title: "Covid-19, the BTA & you",
+        text: `We’re all following the news and have concerns re: Covid-19. This is what we're doing at the BTA to continue to support you.`,
+      },
+    ],
+    btns: {
+      share: "Share",
+      read: "Read More",
+    },
+  }),
   computed: {
     exactRouteIsHome() {
       const [homePath1, homePath2] = ["/", "/home"];
