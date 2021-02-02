a584d80272c0eb081f15a860bbc608a15a4321b5
Edward Casanova <cb.inv7613@gmail.com>
2021-02-02 08:29:57 -0600

    added image, and breadcrumbs. There's an issue with the breadcrumbs. Working on that

--- src/components/ViewsChildren/AboutUs/Children.vue
+++ src/components/ViewsChildren/AboutUs/Children.vue
@@ -1,12 +1,49 @@
 <template>
   <v-container ref="about-children-container" fluid class="temp-border">
-    <v-row no-gutters class="temp-border__item"> </v-row>
+    <!-- 1st row -->
+    <v-row no-gutters class="temp-border__item ma-3 pa-3">
+      <v-img
+        :aspect-ratio="16 / 9"
+        src="https://images.unsplash.com/photo-1455849318743-b2233052fcff?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
+        height="50vh"
+        position="center center"
+        class="temp-border__item"
+      >
+      </v-img>
+    </v-row>
+
+    <!-- 2nd row -->
+    <v-row no-gutters class="temp-border__item ma-3 pa-3">
+      <v-breadcrumbs :items="breadcrumbItems">
+        <template #divider>
+          <v-icon>mdi-chevron-right</v-icon>
+        </template>
+      </v-breadcrumbs>
+    </v-row>
   </v-container>
 </template>
 
 <script>
+import { mapState } from "vuex";
+import { scrollToTop as scrollToTopUtil } from "../../../utils/scrollToTop.js";
+
 export default {
   name: "AboutUsChildren",
+  computed: {
+    ...mapState({
+      breadcrumbItems: state => state.CentralState.currentBreadcrumbs,
+    }),
+  },
+  methods: {
+    scrollToTop() {
+      scrollToTopUtil(this, "about-children-container");
+    },
+  },
+  beforeRouteEnter(to, from, next) {
+    next(vm => {
+      vm.$store.commit({ type: "createBreadcrumbs", component: vm });
+    });
+  },
 };
 </script>
 
