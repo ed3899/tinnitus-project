f6f844ea97260351316d8f8ff58f8d2a88a2909f
Edward Casanova <cb.inv7613@gmail.com>
2021-02-04 10:40:08 -0600

    Changes to be committed: 	modified:   src/components/ViewsChildren/AboutUs/Children.vue
    - Added scroll to top on Children.vue

--- src/components/ViewsChildren/AboutUs/Children.vue
+++ src/components/ViewsChildren/AboutUs/Children.vue
@@ -108,6 +108,9 @@ export default {
       }
     },
   },
+  beforeUpdate() {
+    this.scrollToTop();
+  },
   methods: {
     scrollToTop() {
       scrollToTopUtil(this, "about-children-container");
