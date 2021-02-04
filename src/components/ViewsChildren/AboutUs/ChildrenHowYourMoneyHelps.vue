f4b7da77e10b2d9fbb40a477330b7cb7fa51207b
Edward Casanova <cb.inv7613@gmail.com>
2021-02-04 11:50:28 -0600

    Changes to be committed: 	modified:   src/components/ViewsChildren/AboutUs/ChildrenHowYourMoneyHelps.vue - Initial modifications to this file - Some basic layout stuff

--- src/components/ViewsChildren/AboutUs/ChildrenHowYourMoneyHelps.vue
+++ src/components/ViewsChildren/AboutUs/ChildrenHowYourMoneyHelps.vue
@@ -1,7 +1,21 @@
 <template>
-  <div>
-    how osadfas
-  </div>
+  <v-container class="temp-border pa-1">
+    <v-row no-gutters class="temp-border__item pa-3 ma-3">
+      <h2 class="text-h2">How your money helps</h2>
+    </v-row>
+
+    <v-row no-gutters class="temp-border__item pa-3 ma-3">
+      <p class="text-body-1">
+        Our work is paid for by the generosity of members and supporters like
+        you.
+      </p>
+      <p class="text-body-1">
+        We receive no money directly from the government, so we are reliant on
+        the generosity and support of members of the public and grant-making
+        organisations to be able to support people with tinnitus across the UK.
+      </p>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
