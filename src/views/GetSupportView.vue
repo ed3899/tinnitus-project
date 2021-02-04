0041ba69e46b2db77823edfad596dd4f781ade55
Edward Casanova <cb.inv7613@gmail.com>
2021-02-04 17:42:12 -0600

    On branch get-support  Changes to be committed: 	new file:   src/components/ViewsChildren/GetSupport/Main.vue 	modified:   src/views/GetSupportView.vue
    - Working on main content
    - Evaluating the functionality of data-tables

--- src/views/GetSupportView.vue
+++ src/views/GetSupportView.vue
@@ -1,9 +1,72 @@
 <template>
-  <v-container>
-    <v-row>
-      <h1>Get Support</h1>
+  <v-container class="temp-border mt-n6 pa-3" fluid>
+    <!-- 1st row -->
+    <v-row no-gutters class="temp-border__item ma-3 pa-3">
+      <v-row no-gutters class="temp-border__item ma-3 pa-3">
+        <h2 class="text-h2">How loud is loud?</h2>
+      </v-row>
+
+      <v-row no-gutters class="temp-border__item ma-3 pa-3">
+        <p class="text-body-2">
+          If you are trying to understand how loud sounds are, and what is and
+          isn’t safe then this information should help.
+        </p>
+
+        <p class="text-body-2">
+          When thinking about sound levels you also need to be aware of exposure
+          times, so how long sounds are safe for, this will help you understand
+          when and how to protect yourself.
+        </p>
+      </v-row>
     </v-row>
-    <v-row>
+
+    <!-- 2nd row -->
+    <v-row no-gutters class="temp-border__item ma-3 pa-3">
+      <v-row no-gutters class="temp-border__item ma-3 pa-3">
+        <h2 class="text-h2">What is a decibel?</h2>
+      </v-row>
+      <v-row no-gutters class="temp-border__item ma-3 pa-3">
+        <p class="text-body-2">
+          A decibel dB is the unit used to measure the intensity of a sound –
+          85dB and above is the level at which noise becomes unsafe without the
+          use of hearing protection. 85dB and over hearing protection should be
+          provided.
+        </p>
+
+        <p class="text-body-2">
+          For sounds under 85dB, there should be no need for hearing protection.
+          Although if you work in a noisy environment, up to 80dB, you should be
+          trained and educated to understand the risks and hearing protection
+          should be made available.
+        </p>
+
+        <p class="text-body-2">
+          These rules do not relate to social noise, and that’s where you need
+          to make sure you protect yourself.
+        </p>
+      </v-row>
+    </v-row>
+
+    <!-- 3rd row -->
+    <v-row no-gutters class="temp-border__item ma-3 pa-3">
+      <v-row no-gutters class="temp-border__item ma-3 pa-3">
+        <h2 class="text-h2">Sound levels and exposure times</h2>
+      </v-row>
+
+      <v-row no-gutters class="temp-border__item ma-3 pa-3">
+        <p class="text-body-1">
+          Here are some examples of sound levels and maximum exposure times,
+          after which hearing protection is required. These examples should give
+          you a gauge for sound levels, some sounds are unavoidable and we
+          aren't saying avoid the below sounds - for example, you can't avoid a
+          crying baby, but it's good to know how to understand what
+        </p>
+      </v-row>
+
+      <v-row no-gutters class="temp-border__item ma-3 pa-3"></v-row>
+    </v-row>
+
+    <v-row no-gutters class="temp-border__item ma-3 pa-3">
       <router-view></router-view>
     </v-row>
   </v-container>
