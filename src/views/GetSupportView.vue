5726694fea8888668ab9b090acaf83788987fb27
Edward Casanova <cb.inv7613@gmail.com>
2021-02-05 09:44:29 -0600

    On branch get-support  Changes to be committed: 	modified:   src/views/GetSupportView.vue
    - Added data-table with iteration

--- src/views/GetSupportView.vue
+++ src/views/GetSupportView.vue
@@ -63,7 +63,16 @@
         </p>
       </v-row>
 
-      <v-row no-gutters class="temp-border__item ma-3 pa-3"></v-row>
+      <v-row no-gutters class="temp-border__item ma-3 pa-3">
+        <v-data-table
+          :headers="headers"
+          :items="items"
+          :items-per-page="5"
+          dense
+          class="elevation-8"
+        >
+        </v-data-table>
+      </v-row>
     </v-row>
 
     <v-row no-gutters class="temp-border__item ma-3 pa-3">
@@ -75,6 +84,76 @@
 <script>
 export default {
   name: "GetSupportView",
+  data: () => ({
+    headers: [
+      {
+        text: "Decibel",
+        align: "start",
+        sortable: false,
+        value: "decibels",
+      },
+      {
+        text: "Source",
+        value: "source",
+      },
+      {
+        text: "Recommended exposure time",
+        value: "hours",
+      },
+    ],
+    items: [
+      {
+        decibels: "85db",
+        source: "Kitchen blender",
+        hours: "8 hours",
+      },
+      {
+        decibels: "88dB",
+        source: "Forklift truck",
+        hours: "4 hours",
+      },
+      {
+        decibels: "91dB",
+        source: "Tube train",
+        hours: "2 hours",
+      },
+      {
+        decibels: "94dB",
+        source: "Lawnmower",
+        hours: "1 hours",
+      },
+      {
+        decibels: "97dB",
+        source: "Industrial fire alarm",
+        hours: "0.5 hours or '30 mins'",
+      },
+      {
+        decibels: "100dB",
+        source: "Handheld drill",
+        hours: "0.25 hours or '15 min'",
+      },
+      {
+        decibels: "103dB",
+        source: "MP3 player max vol",
+        hours: "0.125 hours or '7.5 mins'",
+      },
+      {
+        decibels: "106dB",
+        source: "Motorbike",
+        hours: "0.0625 hours or '3.75 mins'",
+      },
+      {
+        decibels: "109dB",
+        source: "Crying baby",
+        hours: "0.03 hours or '112 secs'",
+      },
+      {
+        decibels: "112dB",
+        source: "Live rock band",
+        hours: "0.01 hours or '66 secs'",
+      },
+    ],
+  }),
 };
 </script>
 
