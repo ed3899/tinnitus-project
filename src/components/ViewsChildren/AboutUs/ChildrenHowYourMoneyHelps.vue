7c8cd5f1a7cb2662b1dfbce42804dc7071e7a650
Edward Casanova <cb.inv7613@gmail.com>
2021-02-10 09:54:45 -0600

    -Migrated all dummy data to Vuex - Thinking about migrating all static data there as well

--- src/components/ViewsChildren/AboutUs/ChildrenHowYourMoneyHelps.vue
+++ src/components/ViewsChildren/AboutUs/ChildrenHowYourMoneyHelps.vue
@@ -3,13 +3,13 @@
     <!-- 1st row -->
     <v-row no-gutters class="temp-border__item pa-3 ma-3">
       <v-row no-gutters class="temp-border__item pa-3 ma-3">
-        <h2 v-text="staticContent.firstRow.title" class="text-h2"></h2>
+        <h2 v-text="firstRow.title" class="text-h2"></h2>
       </v-row>
 
       <v-row no-gutters class="temp-border__item pa-3 ma-3">
-        <p v-text="staticContent.firstRow.paragraphOne" class="text-body-1"></p>
+        <p v-text="firstRow.paragraphOne" class="text-body-1"></p>
 
-        <p v-text="staticContent.firstRow.paragraphTwo" class="text-body-1"></p>
+        <p v-text="firstRow.paragraphTwo" class="text-body-1"></p>
       </v-row>
     </v-row>
 
@@ -18,7 +18,7 @@
     <!-- 2nd row -->
     <v-row no-gutters class="temp-border__item pa-3 ma-3">
       <v-row no-gutters class="temp-border__item pa-3 ma-3">
-        <h2 v-text="staticContent.secondRow.title" class="text-h2"></h2>
+        <h2 v-text="secondRow.title" class="text-h2"></h2>
       </v-row>
 
       <v-row no-gutters class="temp-border__item pa-3 ma-3">
@@ -40,43 +40,27 @@
         cols="12"
         class="temp-border__item pa-3 d-flex justify-space-around align-center"
       >
-        <p
-          v-text="staticContent.thirdRow.paragraph"
-          class="ma-0 text-body-2"
-        ></p>
+        <p v-text="thirdRow.paragraph" class="ma-0 text-body-2"></p>
 
-        <v-btn v-text="staticContent.thirdRow.btn" color="success"> </v-btn>
+        <v-btn v-text="thirdRow.btn" color="success"> </v-btn>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
-import { data as dummyHighlightsLoader } from "../../../data/AboutUsHowYourMoneyHelps.js";
+import { mapState } from "vuex";
 
 export default {
   name: "ChildrenHowYourMoneyHelps",
-  data: () => ({
-    highlights: dummyHighlightsLoader(),
-    staticContent: {
-      firstRow: {
-        title: "How your money helps",
-        paragraphOne: `Our work is paid for by the generosity of members and supporters like
-          you.`,
-        paragraphTwo: ` We receive no money directly from the government, so we are reliant on
-          the generosity and support of members of the public and grant-making
-          organisations to be able to support people with tinnitus across the
-          UK.`,
-      },
-      secondRow: {
-        title: "2020 Highlights",
-      },
-      thirdRow: {
-        paragraph: "Download our latest annual review to read more",
-        btn: "Our impact",
-      },
-    },
-  }),
+  computed: {
+    ...mapState("dummyData", {
+      highlights: state => state.About.HowYourMoneyHelps.highlights,
+      firstRow: state => state.About.HowYourMoneyHelps.firstRow,
+      secondRow: state => state.About.HowYourMoneyHelps.secondRow,
+      thirdRow: state => state.About.HowYourMoneyHelps.thirdRow,
+    }),
+  },
 };
 </script>
 
