9060e90ff4ca149d5c50c62883af1ff7638ab37a
Edward Casanova <cb.inv7613@gmail.com>
2021-02-09 18:13:55 -0600

    -Finished adding data to expansion panel iterator - Styles at the end of the process

--- src/components/ExpansionPanelIterator/ExpansionPanelIterator.vue
+++ src/components/ExpansionPanelIterator/ExpansionPanelIterator.vue
@@ -1,14 +1,39 @@
 <template>
-  <v-data-iterator :items="itemsArray">
+  <v-data-iterator
+    :items="itemsArray"
+    :items-per-page.sync="itemsPerPage"
+    :page.sync="page"
+    :search="search"
+    :footer-props="{ itemsPerPageOptions: itemsPerPageArray }"
+  >
+    <template #header>
+      <v-row no-gutters class="temp-border__item pa-1 ma-1">
+        <v-text-field
+          v-model="search"
+          clearable
+          flat
+          solo-inverted
+          hide-details
+          prepend-inner-icon="mdi-magnify"
+          label="Search"
+        ></v-text-field>
+      </v-row>
+    </template>
+
     <template #default="{items : teamArray}">
-      <v-row no-gutters>
+      <v-row no-gutters class="temp-border__item pa-1 ma-1">
         <v-expansion-panels>
           <v-expansion-panel
             v-for="{ name, bio } in teamArray"
             :key="name + bio"
           >
-            <v-expansion-panel-header v-text="name"></v-expansion-panel-header>
-            <v-expansion-panel-content v-text="bio"></v-expansion-panel-content>
+            <v-expansion-panel-header :expand-icon="icons.menuDown">
+              {{ name }}
+            </v-expansion-panel-header>
+            <v-expansion-panel-content
+              v-text="bio"
+              class="pl-5 pb-3"
+            ></v-expansion-panel-content>
           </v-expansion-panel>
         </v-expansion-panels>
       </v-row>
@@ -29,6 +54,10 @@ export default {
     icons: {
       menuDown: "mdi-menu-down",
     },
+    itemsPerPageArray: [3, 5, 8, -1],
+    search: "",
+    page: 1,
+    itemsPerPage: 5,
   }),
 };
 </script>
