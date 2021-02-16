e7bc94bf462296365096c635b598f3f91f3ba321
Edward Casanova <cb.inv7613@gmail.com>
2021-02-16 11:01:46 -0600

    - How your money helps - dev styles added

--- src/components/ChildrenBaseLayout/About_HowYourMoneyHelps.vue
+++ src/components/ChildrenBaseLayout/About_HowYourMoneyHelps.vue
@@ -1,27 +1,57 @@
 <template>
-  <v-container class="temp-border pa-1">
-    <!-- 1st row -->
-    <v-row no-gutters class="temp-border__item pa-1 ma-1">
-      <v-row no-gutters class="temp-border__item pa-1 ma-1">
-        <h2 v-text="firstRow.title" class="text-h2"></h2>
+  <v-container :style="[weAreOnDevMode ? brownBorder : '']" class="pa-1">
+    <!-- Text -->
+    <v-row no-gutters :style="[weAreOnDevMode ? brownBorder : '']" class="pa-1">
+      <v-row
+        no-gutters
+        :style="[weAreOnDevMode ? greenBorder : '']"
+        class="pa-1"
+      >
+        <h2 class="text-h2">How your money helps</h2>
       </v-row>
 
-      <v-row no-gutters class="temp-border__item pa-1 ma-1">
-        <p v-text="firstRow.paragraphOne" class="text-body-1"></p>
+      <v-row
+        no-gutters
+        :style="[weAreOnDevMode ? greenBorder : '']"
+        class="pa-1 mt-1"
+      >
+        <p class="text-body-1">
+          Our work is paid for by the generosity of members and supporters like
+          you.
+        </p>
 
-        <p v-text="firstRow.paragraphTwo" class="text-body-1"></p>
+        <p class="text-body-1">
+          We receive no money directly from the government, so we are reliant on
+          the generosity and support of members of the public and grant-making
+          organisations to be able to support people with tinnitus across the
+          UK.
+        </p>
       </v-row>
     </v-row>
 
     <v-divider></v-divider>
 
-    <!-- 2nd row -->
-    <v-row no-gutters class="temp-border__item pa-1 ma-1">
-      <v-row no-gutters class="temp-border__item pa-1 ma-1">
-        <h2 v-text="secondRow.title" class="text-h2"></h2>
+    <!-- Highlights-->
+    <v-row
+      no-gutters
+      :style="[weAreOnDevMode ? brownBorder : '']"
+      class="pa-1 my-1"
+    >
+      <!-- Title -->
+      <v-row
+        no-gutters
+        :style="[weAreOnDevMode ? greenBorder : '']"
+        class="pa-1 mb-1"
+      >
+        <h2 class="text-h2">2020 Highlights</h2>
       </v-row>
 
-      <v-row no-gutters class="temp-border__item pa-1 ma-1">
+      <!-- Content -->
+      <v-row
+        no-gutters
+        :style="[weAreOnDevMode ? greenBorder : '']"
+        class="pa-1"
+      >
         <ul>
           <li
             v-for="{ content } in highlights"
@@ -34,34 +64,55 @@
 
     <v-divider></v-divider>
 
-    <!-- 3rd row -->
-    <v-row no-gutters class="temp-border__item pa-1 ma-1">
+    <!-- Mini footer -->
+    <v-row no-gutters :style="[weAreOnDevMode ? brownBorder : '']" class="pa-1">
       <v-col
         cols="12"
-        class="temp-border__item pa-1 d-flex justify-space-around align-center"
+        :style="[weAreOnDevMode ? greenBorder : '']"
+        class="pa-1 d-flex justify-space-around align-center"
       >
-        <p v-text="thirdRow.paragraph" class="ma-0 text-body-2"></p>
+        <p class="ma-0 text-body-2">
+          Download our latest annual review to read more
+        </p>
 
-        <v-btn v-text="thirdRow.btn" color="success"> </v-btn>
+        <v-btn color="success">Our impact</v-btn>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
-import { mapState } from "vuex";
+//% Utils
+import { weAreOnDevMode, brownBorder, greenBorder } from "../../utils/index";
 
 export default {
   name: "AboutHowYourMoneyHelps",
+  data: () => ({
+    highlights: [
+      {
+        content: `Hold 2 tinnitus debates in parliament as a result of the Tinnitus Roundtable event and Manifesto`,
+      },
+      {
+        content: `Continue to invest in research projects to find out new management techniques for tinnitus, and lead to an eventual cure`,
+      },
+      {
+        content: `Achieve 103,000 signatures on our petition urging the government to commit more funding to tinnitus research`,
+      },
+      {
+        content: `Welcome 1,291,933 unique visitors to our website - providing reliable information about tinnitus`,
+      },
+      {
+        content: `Answer 6,141 freephone helpline calls from members of the public needing support with their tinnitus or wanting to help a friend or relative`,
+      },
+      {
+        content: `Provide training for 224 professionals through our events, workshops and lectures - helping to improve the quality of support offered to tinnitus patients`,
+      },
+    ],
+  }),
   computed: {
-    ...mapState("dummyData", {
-      highlights: state => state.About.HowYourMoneyHelps.highlights,
-      firstRow: state => state.About.HowYourMoneyHelps.firstRow,
-      secondRow: state => state.About.HowYourMoneyHelps.secondRow,
-      thirdRow: state => state.About.HowYourMoneyHelps.thirdRow,
-    }),
+    weAreOnDevMode,
+    brownBorder,
+    greenBorder,
   },
 };
 </script>
-
-<style lang="scss" scoped></style>
--- src/store/modules/dummyData.js
+++ src/store/modules/dummyData.js
@@ -341,35 +341,6 @@ export const module = {
         },
       ],
       HowYourMoneyHelps: {
-        highlights: [
-          {
-            content: `Hold 2 tinnitus debates in parliament as a result of the Tinnitus Roundtable event and Manifesto`,
-          },
-          {
-            content: `Continue to invest in research projects to find out new management techniques for tinnitus, and lead to an eventual cure`,
-          },
-          {
-            content: `Achieve 103,000 signatures on our petition urging the government to commit more funding to tinnitus research`,
-          },
-          {
-            content: `Welcome 1,291,933 unique visitors to our website - providing reliable information about tinnitus`,
-          },
-          {
-            content: `Answer 6,141 freephone helpline calls from members of the public needing support with their tinnitus or wanting to help a friend or relative`,
-          },
-          {
-            content: `Provide training for 224 professionals through our events, workshops and lectures - helping to improve the quality of support offered to tinnitus patients`,
-          },
-        ],
-        firstRow: {
-          title: "How your money helps",
-          paragraphOne: `Our work is paid for by the generosity of members and supporters like
-            you.`,
-          paragraphTwo: ` We receive no money directly from the government, so we are reliant on
-            the generosity and support of members of the public and grant-making
-            organisations to be able to support people with tinnitus across the
-            UK.`,
-        },
         secondRow: {
           title: "2020 Highlights",
         },
