0bfcdd08272f5f18cd36e0707f6078de09d3a6df
Edward Casanova <cb.inv7613@gmail.com>
2021-02-20 14:39:13 -0600

    -Done!

--- src/components/NavBar/NavBar_NavDrawer.vue
+++ src/components/NavBar/NavBar_NavDrawer.vue
@@ -1,38 +1,87 @@
 <template>
   <v-navigation-drawer v-model="value" app temporary width="75vw">
     <v-list nav>
-      <!-- Parent -->
-      <v-list-group
+      <template
         v-for="({ path: parentPath, name, icon: parentIcon, children },
         i) in normalizedRoutes"
-        :key="name + parentPath + i"
-        no-action
-        @click="goToRoute(parentPath)"
-        :prepend-icon="parentIcon"
-        :value="currentRoute === parentPath"
-        :group="parentPath"
       >
-        <template #activator>
-          <v-list-item-content>
-            <v-list-item-title v-text="name"></v-list-item-title>
-          </v-list-item-content>
-        </template>
-
-        <v-list-item
-          v-for="({ path: childPath, name }, i) in children"
-          :key="childPath + name + i"
-          :to="{ path: `${parentPath}/${childPath}` }"
-          exact
-          exact-active-class="teal accent-1 black--text"
+        <!-- Parent home -->
+        <v-list-group
+          v-if="parentPath === '/home'"
+          :key="name + parentPath + i"
+          no-action
+          :prepend-icon="parentIcon"
         >
-          <v-list-item-content>
-            <v-list-item-title
-              v-text="name"
-              class="text-capitalize"
-            ></v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list-group>
+          <template #prependIcon>
+            <v-icon @click="goToRoute(parentPath)" v-text="parentIcon"></v-icon>
+          </template>
+
+          <!-- :group="'^\/$|^\/home'" -->
+          <template #activator>
+            <v-list-item-content @click="goToRoute(parentPath)">
+              <v-list-item-title v-text="name"></v-list-item-title>
+            </v-list-item-content>
+          </template>
+
+          <template #appendIcon>
+            <v-icon>mdi-chevron-down</v-icon>
+          </template>
+
+          <v-list-item
+            v-for="({ path: childPath, name }, i) in children"
+            :key="childPath + name + i"
+            :to="{ path: `${parentPath}/${childPath}` }"
+            exact
+            exact-active-class="teal accent-1 black--text"
+          >
+            <v-list-item-content>
+              <v-list-item-title
+                v-text="name"
+                class="text-capitalize"
+              ></v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-group>
+
+        <!-- Parent rest -->
+        <v-list-group
+          v-else
+          :key="name + parentPath + i"
+          no-action
+          :prepend-icon="parentIcon"
+        >
+          <!-- :group="parentPath" -->
+
+          <template #prependIcon>
+            <v-icon @click="goToRoute(parentPath)" v-text="parentIcon"></v-icon>
+          </template>
+
+          <template #activator>
+            <v-list-item-content @click="goToRoute(parentPath)">
+              <v-list-item-title v-text="name"></v-list-item-title>
+            </v-list-item-content>
+          </template>
+
+          <template #appendIcon>
+            <v-icon>mdi-chevron-down</v-icon>
+          </template>
+
+          <v-list-item
+            v-for="({ path: childPath, name }, i) in children"
+            :key="childPath + name + i"
+            :to="{ path: `${parentPath}/${childPath}` }"
+            exact
+            exact-active-class="teal accent-1 black--text"
+          >
+            <v-list-item-content>
+              <v-list-item-title
+                v-text="name"
+                class="text-capitalize"
+              ></v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-group>
+      </template>
     </v-list>
   </v-navigation-drawer>
 </template>
